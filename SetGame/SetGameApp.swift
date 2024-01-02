ce15bf10c72c753d6e2e60db7795e7cbdd44cbe6
Vosik <vosikvos@gmail.com>
2024-01-02 23:25:38 +0200

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Саша Восколович on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
